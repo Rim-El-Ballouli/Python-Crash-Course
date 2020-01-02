8d225ee53ef48f5b798c1add89fb3fc2208b3dad
Rim-El-Ballouli <51033991+Rim-El-Ballouli@users.noreply.github.com>
2020-01-02 15:18:16 +0100

    Update ex_2.6.py

--- solutions/chapter2/ex_2.6.py
+++ solutions/chapter2/ex_2.6.py
@@ -1,5 +1,5 @@
-name = ""
-qoute = ""
+name = "John F. Kennedy "
+qoute = "The greater our knowledge increases the more our ignorance unfolds."
 
 message = name.title() + ' once said, "' + qoute + '"'
 print(message)
