1520a5d891b303e608c323fb583498c13aee30f1
Rim-El-Ballouli <51033991+Rim-El-Ballouli@users.noreply.github.com>
2020-01-03 09:57:52 +0100

    Add files via upload

---
+++ solutions/chapter4/ex_4.13.py
@@ -0,0 +1,9 @@
+buffet = ('Burger', 'Mushroom soup', 'Cheese sticks', 'Fries')
+for item in buffet:
+    print(item)
+
+buffet[0] = 'New item'
+
+buffet = ('Burger', 'Mushroom soup', 'Cheese sandwich', 'Pizza')
+for item in buffet:
+    print(item)
