1e60e17560554475464c25bee70ce430108a8080
Rim-El-Ballouli <51033991+Rim-El-Ballouli@users.noreply.github.com>
2020-01-06 09:21:26 +0100

    Update ex_9.7.py

--- solutions/chapter9/ex_9.7.py
+++ solutions/chapter9/ex_9.7.py
@@ -1,12 +1,11 @@
 class User():
-    def __init__(self, first_name, last_name, *privileges):
+    def __init__(self, first_name, last_name):
         self.first_name = first_name
         self.last_name = last_name
         self.login_attempts = 0
-        self.privileges = privileges
 
     def describe_user(self):
-        print('User ' + self.first_name  + ' ' + self.last_name)
+        print('User ' + self.first_name + ' ' + self.last_name)
 
     def greet_user(self):
         print('Hello ' + self.first_name)
@@ -16,14 +15,15 @@ class User():
 
     def reset_login_attempts(self):
         self.login_attempts = 0
-    
-    def show_privileges()
 
 
 class Admin(User):
     def __init__(self, firstN, lastN, *privileges):
-        super().__init__(firstN,lastN)
-        self.privileges = Privileges(privileges)
+        super().__init__(firstN, lastN)
+        self.privileges = privileges
 
-usrAdmin = Admin('fn', 'ln', "can add post", "can delete post", "can ban user")
-usrAdmin.privileges.show_privileges()
+    def show_privileges(self):
+        print(self.privileges)
+
+usrAdmin = Admin('Adrean', 'Park', "can add post", "can delete post", "can ban user")
+usrAdmin.show_privileges()
