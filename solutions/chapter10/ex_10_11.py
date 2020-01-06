6bb58ef7f3aed3158c545f209dfe79877c06c9e4
Rim-El-Ballouli <51033991+Rim-El-Ballouli@users.noreply.github.com>
2020-01-06 15:13:27 +0100

    Update ex_10_11.py

--- solutions/chapter10/ex_10_11.py
+++ solutions/chapter10/ex_10_11.py
@@ -1,9 +1,9 @@
 import json
 
 number = input('Enter your favorite number ')
-with open('favorite_number.txt', 'w') as file_object:
+with open('favorite_number.json', 'w') as file_object:
     json.dump(number, file_object)
 
-with open('favorite_number.txt', 'r') as file_object:
+with open('favorite_number.json', 'r') as file_object:
     fav_num = json.load(file_object)
     print('I know your favorite number ' + fav_num)
